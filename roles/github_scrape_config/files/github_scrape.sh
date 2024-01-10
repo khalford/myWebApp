d797e12e14ecc568a77623fbfecf83b68ab7b37f
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2024-01-10 13:27:19 +0000

    Removed token

--- roles/github_scrape_config/files/github_scrape.sh
+++ roles/github_scrape_config/files/github_scrape.sh
@@ -3,7 +3,7 @@
 while true
 do
   # Get data
-  curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -H "Authorization: Bearer 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" "https://api.github.com/repos/stfc/SCD-Openstack-Utils/contributors?per_page-100" | grep 'contributions\|login\|html_url\|avatar_url' | paste -d "" - - - - > /home/github_scrape/SCD_Openstack_Utils_commits
+  curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -H "Authorization: Bearer <YOUR TOKEN>" "https://api.github.com/repos/stfc/SCD-Openstack-Utils/contributors?per_page-100" | grep 'contributions\|login\|html_url\|avatar_url' | paste -d "" - - - - > /home/github_scrape/SCD_Openstack_Utils_commits
 
   # Format data
   sed -i 's/"//g' /home/github_scrape/SCD_Openstack_Utils_commits
@@ -15,7 +15,7 @@ do
   sed -i 's/,/, /g' /home/github_scrape/SCD_Openstack_Utils_commits
 
   # Get data
-  curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -H "Authorization: Bearer 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" "https://api.github.com/repos/stfc/st2-cloud-pack/contributors?per_page-100" | grep 'contributions\|login\|html_url\|avatar_url' | paste -d "" - - - - > /home/github_scrape/st2_cloud_pack_commits
+  curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" -H "Authorization: Bearer <YOUR TOKEN>" "https://api.github.com/repos/stfc/st2-cloud-pack/contributors?per_page-100" | grep 'contributions\|login\|html_url\|avatar_url' | paste -d "" - - - - > /home/github_scrape/st2_cloud_pack_commits
 
   # Format data
   sed -i 's/"//g' /home/github_scrape/st2_cloud_pack_commits
