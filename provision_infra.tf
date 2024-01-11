4f2e95f03b7bb80d36ece6e5b312c16a70efad0a
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2024-01-11 14:21:42 +0000

    terraform network works.

--- provision_infra.tf
+++ provision_infra.tf
@@ -26,7 +26,7 @@ resource "openstack_networking_subnet_v2" "subnet_1" {
 
 resource "openstack_networking_router_v2" "router_1" {
   name = "web-router"
-  external_network_id = "0dc30001-edfb-4137-be76-8e51f38fd650"
+  external_network_id = "5283f642-8bd8-48b6-8608-fa3006ff4539"
 }
 
 resource "openstack_networking_router_interface_v2" "router_interface_1" {
@@ -37,7 +37,7 @@ resource "openstack_networking_router_interface_v2" "router_interface_1" {
 locals {
   count = 2
   count_set = toset([for v in range(local.count) : tostring(v)])
-  floating_ip = "130.246.211.147"
+  floating_ip = ""
 }
 
 data "openstack_images_image_v2" "ubuntu-2004-nogui" {
@@ -55,7 +55,7 @@ resource "openstack_compute_instance_v2" "web_vm" {
         image_id        = data.openstack_images_image_v2.ubuntu-2004-nogui.id
         flavor_id       = data.openstack_compute_flavor_v2.vm_flavor.flavor_id
         security_groups = ["default", "HTTP"]
-        key_pair        = "ansible-instance"
+        key_pair        = "ansible-instance-pubkey"
     
     network {
         name = openstack_networking_network_v2.network_1.name
@@ -63,7 +63,7 @@ resource "openstack_compute_instance_v2" "web_vm" {
     }
 
     metadata = {
-        group = "web-vm"
+        group = "web-vms"
     }
 }
 
@@ -72,13 +72,13 @@ data "openstack_compute_instance_v2" "web_vm_data" {
   id = "${openstack_compute_instance_v2.web_vm[each.key].id}"
 }
 
-# resource "null_resource" "run_ansible_on_vms" {
-#   for_each = local.count_set
-#   depends_on = [openstack_networking_floatingip_associate_v2.lb_fip, openstack_compute_instance_v2.web_vm]
-#   provisioner "local-exec" {
-#     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i openstack.yml -l ${data.openstack_compute_instance_v2.web_vm_data[each.key].name} deploy_webserver.yaml"
-#   }
-# }
+resource "null_resource" "run_ansible_on_vms" {
+  for_each = local.count_set
+  depends_on = [openstack_networking_floatingip_associate_v2.lb_fip, openstack_compute_instance_v2.web_vm]
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -e 'ansible_port=${openstack_lb_listener_v2.ssh_listener[each.key].protocol_port}' -i ${local.floating_ip},  deploy_webserver.yaml"
+  }
+}
 
 resource "openstack_lb_loadbalancer_v2" "web_loadbalancer" {
   name = "web-loadbalancer"
