4a2e27517844efe3aea3060c01a898619b28fc92
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2024-01-26 11:54:37 +0000

    ssl

--- bastion_ansible_setup/roles/github_scrape_config/files/github_scrape.py
+++ bastion_ansible_setup/roles/github_scrape_config/files/github_scrape.py
@@ -3,7 +3,7 @@
 from typing import List, Dict
 from time import sleep
 from requests import get
-from mysql.connector import connect
+from pymysql import connect
 
 def get_data(TOKEN: str, url: str) -> List[Dict]:
     headers = {'Authorization': 'token ' + TOKEN}
@@ -28,17 +28,19 @@ def update_table(
     with connect(host=host, ssl_ca=ssl_ca, ssl_cert=ssl_cert, ssl_key=ssl_key, ssl_verify_cert=True) as cnx:
         cursor = cnx.cursor()
         for item in data:
-            cursor.execute(f'INSERT INTO {database}.{table} (username, avatar_url, html_url, no_commits) VALUES ("{item["username"]}","{item["avatar"]}", "{item["profile"]}", "{item["contributions"]}") ON DUPLICATE KEY UPDATE username="{item["username"]}", no_commits="{item["contributions"]}";')
+            QUERY = f'INSERT INTO {database}.{table} (username, avatar_url, html_url, no_commits) VALUES ("{item["username"]}","{item["avatar"]}", "{item["profile"]}", "{item["contributions"]}") ON DUPLICATE KEY UPDATE username="{item["username"]}", no_commits="{item["contributions"]}";'
+            cursor.execute(QUERY)
         cnx.commit()
 
-def repo_func(TOKEN: str, url: str, host: str, database: str, table: str, ssl_ca: str, ssl_cert: str, ssl_key: str):
+def repo_func(TOKEN: str, host: str, url: str, database: str, table: str, ssl_ca: str, ssl_cert: str, ssl_key: str):
     response = get_data(TOKEN, url)
     pruned_response = prune_data(response)
     update_table(host=host, database=database, table=table, data=pruned_response, ssl_ca=ssl_ca, ssl_cert=ssl_cert, ssl_key=ssl_key)
 
 
 if __name__ == "__main__":
-    TOKEN = ""
+    TOKEN = "<YOUR_TOKEN>"
+    host = "localhost"
     # Create repo maps here and add to the list
     ssl_paths = {
         "ssl_ca": "/etc/mysql/ca.crt",
@@ -47,19 +49,17 @@ if __name__ == "__main__":
         }
     SCD_Openstack_Utils = {
         "url": "https://api.github.com/repos/stfc/SCD-Openstack-Utils/contributors?per_page-100",
-        "host": "localhost",
         "database": "github_stats",
         "table": "scd_table",
     }
     st2_cloud_pack = {
         "url": "https://api.github.com/repos/stfc/st2-cloud-pack/contributors?per_page-100",
-        "host": "localhost",
         "database": "github_stats",
         "table": "st2_table",
     }
     repos = [SCD_Openstack_Utils, st2_cloud_pack]
     while True:
         for repo in repos:
-            repo_func(TOKEN, **ssl_paths, **repo)
+            repo_func(TOKEN, host, **ssl_paths, **repo)
         sleep(60)
         
